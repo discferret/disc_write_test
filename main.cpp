721b91d09f513914645da17838f1d7f1e3a18e51
Philip Pemberton <philpem@philpem.me.uk>
2013-07-27 03:11:19 +0100

    start encoding actual mfm data

--- main.cpp
+++ main.cpp
@@ -99,6 +99,81 @@ class Track {
 		}
 };
 
+class BitTrack {
+	private:
+		bool _last_data_bit;
+
+	public:
+		vector<bool> _buf;
+
+		void reset(void)
+		{
+			_buf.clear();
+			_last_data_bit = false;
+		}
+
+		void raw(uint32_t data, size_t nbits=16)
+		{
+			assert(nbits <= 32);
+
+			for (unsigned int i=1; i <= nbits; i++) {
+				_buf.push_back(data & (1 << (nbits-i)));
+			}
+			_last_data_bit = data & 1;
+		}
+
+		void mfm(uint8_t data)
+		{
+			for (unsigned int i=1; i <= 8; i++) {
+				bool this_bit = data & (1 << (8 - i));
+
+				// clock = prev NOR curr
+				_buf.push_back(!(_last_data_bit | this_bit));
+				// data is always data
+				_last_data_bit = this_bit;
+			}
+		}
+
+		void fm(uint8_t data)
+		{
+			for (unsigned int i=1; i <= 8; i++) {
+				bool this_bit = data & (1 << (8 - i));
+
+				// clock is always true
+				_buf.push_back(true);
+				// data is always data
+				_last_data_bit = this_bit;
+			}
+		}
+
+		/**
+		 * @brief Convert the contents of the BitTrack into a track.
+		 *
+		 * @param t Track class
+		 * @param cell_time Time for one bit cell
+		 */
+		void toTrack(Track &t, uint32_t cell_time)
+		{
+			uint32_t nzeroes = 0;
+
+			for (std::vector<bool>::iterator bit = _buf.begin(); bit != _buf.end(); ++bit) {
+				if (*bit) {
+					/***
+					 * '1' bit. Calculate time delta and emit a flux transition.
+					 * We add 1 here because there's a minimum gap of 1 cell between adjacent flux transitions.
+					 * Each element in the _buf vector is 1 if there is a transition in that cell.
+					 */
+					t.emit_flux((nzeroes + 1) * cell_time);
+					nzeroes = 0;
+				} else {
+					// '0' bit -- increment the "number of zeroes seen" value
+					nzeroes++;
+				}
+			}
+		}
+};
+
+
 int main(void)
 {
 	DISCFERRET_DEVICE_HANDLE *dh;
@@ -130,14 +205,79 @@ int main(void)
 	t.emit_wait_index();
 	t.emit_wait_index();
 
+
+	BitTrack bt;
+	for (int i=0; i<80; i++)	// Pre-index gap (or something like it?)
+		bt.mfm(0x4E);			// "GAP4a"
+	for (int i=0; i<12; i++)
+		bt.mfm(0x00);			// "SYNC"
+
+	for (int i=0; i<3; i++)
+		bt.raw(0x5224);			// 0xC2 with missing clock between 3 and 4 -- before IAM -- Index Mark
+	bt.mfm(0xFC);				// IAM -- Index Address Mark
+	for (int i=0; i<50; i++)
+		bt.mfm(0x4E);			// GAP1 - Postindex Gap
+
+	uint8_t track = 0;
+	uint8_t side = 0;
+	uint8_t crc0, crc1;
+	uint8_t data[256] = {0xff};
+
+	for (int sector=0; sector<26; sector++) {
+		// SYNC
+		for (int i=0; i<12; i++)
+			bt.mfm(0x00);
+
+		// IDAM
+		for (int i=0; i<3; i++)
+			bt.raw(0x4489);		// 0xA1 with missing clock between 4 and 5 -- before IDAM or DAM
+		bt.mfm(0xFE);			// IDAM -- ID Address Mark
+
+		// ID Record starts here
+		bt.mfm(track);
+		bt.mfm(side);
+		bt.mfm(sector);
+		bt.mfm(0x01);	// sector length
+		bt.mfm(crc0);	// CRC16
+		bt.mfm(crc1);	// CRC16
+
+		// GAP2
+		for (int i=0; i<22; i++)
+			bt.mfm(0x4E);
+
+		// SYNC
+		for (int i=0; i<12; i++)
+			bt.mfm(0x00);
+
+		// Data Record starts here
+		for (int i=0; i<3; i++)		// 3x A1-sync
+			bt.raw(0x4489);
+		bt.mfm(0xFB);				// Data Address Mark
+		for (int i=0; i<256; i++)	// Data payload
+			bt.mfm(data[i]);
+		bt.mfm(crc0);				// CRC16
+		bt.mfm(crc1);				// CRC16
+
+		for (int i=0; i<54; i++)	// GAP3 -- Data gap. 66 bytes. (54 + the 12 at the start of sector)
+			bt.mfm(0x4E);
+	}
+
+	for (int i=0; i<598; i++)		// GAP4b -- Postgap
+		bt.mfm(0x4E);
+
+
+	bt.toTrack(t, 200);		// 10ns per clock @ 100MHz
+							// 2us per cell = 2000ns;   2000/10 = 200 counts
+
+/*
 	// Some flux transitions...
 	//for (int i=0; i<20000; i++) {
 	while (t.get_buf(NULL) < 500*1024) {
-		t.emit_flux(300);
-		//t.emit_flux(300);
-		//t.emit_flux(200);
+		t.emit_flux(800);
+		//t.emit_flux(600);
+		t.emit_flux(400);
 	}
-
+*/
 /*
 	t.emit_stop();
 
@@ -205,7 +345,8 @@ int main(void)
 
 	// Turn the disc drive motor on
 	//discferret_reg_poke(dh, DISCFERRET_R_DRIVE_CONTROL, DISCFERRET_DRIVE_CONTROL_DS0 | DISCFERRET_DRIVE_CONTROL_MOTEN | DISCFERRET_DRIVE_CONTROL_DS2);
-	discferret_reg_poke(dh, DISCFERRET_R_DRIVE_CONTROL, DISCFERRET_DRIVE_CONTROL_DS0 | DISCFERRET_DRIVE_CONTROL_DS2);
+	//discferret_reg_poke(dh, DISCFERRET_R_DRIVE_CONTROL, DISCFERRET_DRIVE_CONTROL_DS0 | DISCFERRET_DRIVE_CONTROL_DS2);
+	discferret_reg_poke(dh, DISCFERRET_R_DRIVE_CONTROL, DISCFERRET_DRIVE_CONTROL_DS1 | DISCFERRET_DRIVE_CONTROL_MOTEN);
 	sleep(2);
 	double indx;
 	discferret_get_index_frequency(dh, true, &indx);
